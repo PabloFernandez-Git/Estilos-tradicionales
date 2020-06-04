4bdb25680d995af43f051195da3e302175923730
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-04 14:23:47 -0300

    Preprocesadores

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import './button.css'
+import React from 'react'
+import './button.scss'
+//import buttonStyles from './button.scss'
+
 
 const Button = () => {
     return (
@@ -11,11 +13,25 @@ export default Button;
 
 
 /*
-Estilos externos
+Preprocesadores (SASS)
 
-Se recomienda crear una carpeta por cada componente donde tendremos el componente (Button.jsx) y su respectivo button.css 
-El nombre del componente debe empezar con mayuscula pero el nombre su hoja de estilos NO.
+Previamente tener instalado Yarn
 
+yarn:
+npm i -g yarn
+yarn -v
+
+
+1. Abrir una terminal/consola de comandos
+2. Movernos a la carpeta correcta
+3. instalar SASS con 'npm' o 'yarn'
+
+yarn add node-sass (recomendado!)
+npm i node-sass
+
+4. Debemos cambiar la extension de nuestros '.css' por '.scss' y corregir las importaciones con la nueva extension.
+
+5. Si no funciona debemos relanzar el proyecto.
 
 
 */
