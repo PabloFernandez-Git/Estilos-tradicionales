8a03d68d5d35b22a6347677e612674dbf2b45837
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-04 10:59:59 -0300

    Estilos externos

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './button.css'
+
+const Button = () => {
+    return (
+        <button className='button'>Click Me!</button>
+    );
+}
+
+export default Button;
+
+
+/*
+Estilos externos
+
+Se recomienda crear una carpeta por cada componente donde tendremos el componente (Button.jsx) y su respectivo button.css 
+El nombre del componente debe empezar con mayuscula pero el nombre su hoja de estilos NO.
+
+
+
+*/
